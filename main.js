2d62c39ebfd10e7a2902b4b704040d798350f524
Nirnav-web <76113884+Nirnav-web@users.noreply.github.com>
2021-05-15 09:22:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,27 @@ function preload(){
 }
 function setup(){
     canvas=createCanvas(300,300);
-    canvas.center()
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    posenet=ml5.poseNet(video,modelLoaded())
+    posenet.on('pose',gotPoses)  
 }
 function draw(){
-
+image(video,0,0,300,300);
 }
 function take_snapshot(){
-    save('Mymustache.png');
+    save('Myimage.png');
     
+}
+function modelLoaded(){
+    console.log("posenet is initialized")
+}
+function gotPoses(results){
+    if(results.length<0){
+        console.log(results);
+        console.log("nose x="+result[0].pose.nose.x)
+        console.log("nose y="+result[0].pose.nose.y)
+    }
 }
